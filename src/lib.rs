6dd48e92c734a11512a9553b977b32e38622f360
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-05-07 01:07:19 +0200

    style: split import groups correctly

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub use self::angle_distance::angle_distance;
+pub use self::hsv::Hsv;
+
 mod angle_distance;
 mod hsv;
 pub mod interpolate;
@@ -9,6 +12,3 @@ pub mod light;
 pub mod location;
 #[cfg(feature = "chrono")]
 pub mod time;
-
-pub use angle_distance::angle_distance;
-pub use hsv::Hsv;
